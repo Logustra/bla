cf7dedd16ddf6e8f22f6ff602562bf6f01f31d4a
logustra <logustra@gmail.com>
2016-11-11 07:53:20 +0700

    four push

--- gruntfile.js
+++ gruntfile.js
@@ -1,23 +1,23 @@
 module.exports = function(grunt){
   grunt.loadNpmTasks("grunt-contrib-uglify");
-  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks("grunt-sass");
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-autoprefixer");
   grunt.initConfig ({
     uglify: {
       my_target: {
         files : {
-          "_/js/scripts.js" : ["_/components/js/*.js"]
+          "dist/js/scripts.js" : ["dist/components/js/*.js"]
         }//files
       }//my_target
     },//uglify
     sass: {
       options: {
-        style: "expanded",
+        outputStyle: "expanded",
       },//options
       dev: {
         files: {
-          "_/components/css/styles.css": ["_/components/sass/styles.scss"]
+          "dist/components/css/styles.css": ["dist/components/sass/*.scss", "dist/components/sass/base/*.scss", "dist/components/sass/modules/*.scss", "dist/components/sass/pages/*.scss", "dist/components/sass/layout/*.scss"]
         }//files
       }//dist
     },//sass
@@ -26,25 +26,25 @@ module.exports = function(grunt){
         browser: ["lates 2 versions", "ie 11", "edge", "firefox", "safari", "opera", "chrome"]
       },
       my_target: {
-        src: "_/components/css/styles.css",
-        dest: "_/css/styles.css"
+        src: "dist/components/css/styles.css",
+        dest: "dist/css/styles.css"
       }
     },
     watch : {
       options: { livereload: true },
       scripts: {
-        files : ["_/components/js/*.js"],
+        files : ["dist/components/js/*.js"],
         tasks : ["uglify"]
       },//scripts
       sass: {
-        files : ["_/components/sass/*.scss"],
+        files : ["dist/components/sass/*.scss", "dist/components/sass/base/*.scss", "dist/components/sass/modules/*.scss", "dist/components/sass/pages/*.scss", "dist/components/sass/layout/*.scss"],
         tasks : ["sass"]
       },//sass
       html : {
         files: ["*.html"]
       },//html
       autoprefixer: {
-        files: ["_/components/css/styles.css"],
+        files: ["dist/components/css/styles.css"],
         tasks: ["autoprefixer"]
       },
     }//watch
