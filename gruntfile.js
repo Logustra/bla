6dab87fb443a38dbf21b5e6e9c7c845cbce92131
logustra <logustra@gmail.com>
2016-11-23 09:33:04 +0700

    six push

--- gruntfile.js
+++ gruntfile.js
@@ -3,51 +3,76 @@ module.exports = function(grunt){
   grunt.loadNpmTasks("grunt-sass");
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-autoprefixer");
+  grunt.loadNpmTasks('grunt-browser-sync');
+
   grunt.initConfig ({
+
+    // minify javaScript
     uglify: {
       my_target: {
         files : {
-          "dist/js/scripts.js" : ["dist/components/js/*.js"]
+          "dist/js/scripts.js" : ["dist/builds/js/*.js"]
         }//files
       }//my_target
     },//uglify
+
+    //compile sass
     sass: {
       options: {
-        outputStyle: "expanded",
+        sourceMap: true,
+        outputStyle: "compressed"
       },//options
       dev: {
         files: {
-          "dist/components/css/styles.css": ["dist/components/sass/*.scss", "dist/components/sass/base/*.scss", "dist/components/sass/modules/*.scss", "dist/components/sass/pages/*.scss", "dist/components/sass/layout/*.scss"]
+          "dist/builds/css/styles.css": ["dist/builds/sass/*.scss", "dist/builds/sass/base/*.scss", "dist/builds/sass/modules/*.scss", "dist/builds/sass/pages/*.scss", "dist/builds/sass/layout/*.scss"]
         }//files
       }//dist
     },//sass
+
+    //automatic browser prefixing
     autoprefixer: {
       options: {
-        browser: ["lates 2 versions", "ie 11", "edge", "firefox", "safari", "opera", "chrome"]
+        browser: ["last 6 versions"],
+        cascade: true
       },
       my_target: {
-        src: "dist/components/css/styles.css",
+        src: "dist/builds/css/styles.css",
         dest: "dist/css/styles.css"
       }
     },
+
+    //watch
     watch : {
-      options: { livereload: true },
       scripts: {
-        files : ["dist/components/js/*.js"],
+        files : ["dist/builds/js/*.js"],
         tasks : ["uglify"]
       },//scripts
       sass: {
-        files : ["dist/components/sass/*.scss", "dist/components/sass/base/*.scss", "dist/components/sass/modules/*.scss", "dist/components/sass/pages/*.scss", "dist/components/sass/layout/*.scss"],
+        files : ["dist/builds/sass/*.scss", "dist/builds/sass/base/*.scss", "dist/builds/sass/modules/*.scss", "dist/builds/sass/pages/*.scss", "dist/builds/sass/layout/*.scss"],
         tasks : ["sass"]
       },//sass
       html : {
         files: ["*.html"]
       },//html
       autoprefixer: {
-        files: ["dist/components/css/styles.css"],
+        files: ["dist/builds/css/styles.css"],
         tasks: ["autoprefixer"]
       },
-    }//watch
+    },//watch
+
+    //livereload
+    browserSync: {
+      bsFiles: {
+        src : ["*.html", "dist/css/styles.css", "dist/js/scripts.js"]
+      },
+    options: {
+      watchTask: true,
+        server: {
+            baseDir: "./"
+        }//server
+      }//options
+    }//browserSync
   });//initConfig
-  grunt.registerTask("default", "watch");
+
+  grunt.registerTask("default", ["browserSync", "watch"]);
 };
